69986354eec64a601b63f3feecd1e560b97d1244
michaelpm54 <39412876+michaelpm54@users.noreply.github.com>
2020-11-03 20:26:18 +0000

    Fix windows build

--- src/engine.cpp
+++ src/engine.cpp
@@ -2,66 +2,60 @@
 
 #include <iostream>
 
-#include <SDL2/SDL.h>
-
 #include "gfx_device.h"
 #include "renderer.h"
-#include "window_sdl.h"
 #include "timer.h"
+#include "window_sdl.h"
 #include "world.h"
 
-Engine::Engine() : _timer() {};
+Engine::Engine() : _timer(){};
 
-Engine::~Engine()
-{
-	/* Make sure the GL stuff is released before the context is deleted. */
-	_gfx->Destroy();
-	_win->Destroy();
+Engine::~Engine() {
+  /* Make sure the GL stuff is released before the context is deleted. */
+  _gfx->Destroy();
+  _win->Destroy();
 }
 
-void Engine::Start()
-{
-	Init();
-	Loop();
+void Engine::Start() {
+  Init();
+  Loop();
 }
 
-void Engine::Init()
-{
-	_timer.Reset();
-	_win = std::make_unique<Window_SDL>(WindowSettings{"Engine", 640, 640, false});
-	_win->CreateGLContext(4, 6, false, false);
-	_ren = std::make_shared<Renderer>();
-	_ren->SetClearColour(0, 0.5, 0, 1);
-	_gfx = std::make_shared<GfxDevice>();
-	_world = std::make_shared<World>(_gfx, _ren);
+void Engine::Init() {
+  _timer.Reset();
+  _win =
+      std::make_unique<Window_SDL>(WindowSettings{"Engine", 640, 640, false});
+  _win->CreateGLContext(4, 6, false, false);
+  _ren = std::make_shared<Renderer>();
+  _ren->SetClearColour(0, 0.5, 0, 1);
+  _gfx = std::make_shared<GfxDevice>();
+  _world = std::make_shared<World>(_gfx, _ren);
 
-	std::cout << "Engine init took " << _timer.msElapsedSinceReset() << " ms" << std::endl;
+  std::cout << "Engine init took " << _timer.msElapsedSinceReset() << " ms"
+            << std::endl;
 }
 
-void Engine::Loop()
-{
-	_timer.Reset();
-	int simulationTime = 0;
+void Engine::Loop() {
+  _timer.Reset();
+  int simulationTime = 0;
 
-	_world->Load("assets/worlds/intro.wd");
+  _world->Load("assets/worlds/intro.wd");
 
-	while (!_quitRequested)
-	{
-		int realTime = _timer.msElapsedSinceReset();
+  while (!_quitRequested) {
+    int realTime = _timer.msElapsedSinceReset();
 
-		_world->Input();
+    _world->Input();
 
-		while (simulationTime < realTime)
-		{
-			simulationTime += 16;
-			_world->Update(16);
-		}
+    while (simulationTime < realTime) {
+      simulationTime += 16;
+      _world->Update(16);
+    }
 
-		_quitRequested = _world->quitRequested();
+    _quitRequested = _world->quitRequested();
 
-		_ren->Clear();
-		_world->Render();
-		_win->Update();
-		SDL_Delay(16);
-	}
+    _ren->Clear();
+    _world->Render();
+    _win->Update();
+    SDL_Delay(16);
+  }
 }
--- src/window_sdl.h
+++ src/window_sdl.h
@@ -2,27 +2,32 @@
 #define WINDOW_SDL_H
 
 #define SDL_MAIN_HANDLED
+#ifdef _WIN32
+#include <SDL.h>
+#else
 #include <SDL2/SDL.h>
+#endif
 
 #include "window.h"
 
-class Window_SDL : public Window
-{
+class Window_SDL : public Window {
 public:
-	Window_SDL() = default;
-	Window_SDL(const WindowSettings &s);
-	virtual ~Window_SDL() override;
-	virtual void Create(const WindowSettings &s) override;
-	virtual void Destroy() override;
-	virtual void Update() override;
-	virtual void CreateGLContext(int glVerMajor, int glVerMinor, bool compat, bool doubleBuffer) override;
+  Window_SDL() = default;
+  Window_SDL(const WindowSettings &s);
+  virtual ~Window_SDL() override;
+  virtual void Create(const WindowSettings &s) override;
+  virtual void Destroy() override;
+  virtual void Update() override;
+  virtual void CreateGLContext(int glVerMajor, int glVerMinor, bool compat,
+                               bool doubleBuffer) override;
 
-	SDL_Window *getHandle();
-	SDL_GLContext getGLContext();
+  SDL_Window *getHandle();
+  SDL_GLContext getGLContext();
+
 private:
-	SDL_Window *_handle { nullptr };
-	SDL_GLContext _ctx { nullptr };
-	bool _destroyed { false };
+  SDL_Window *_handle{nullptr};
+  SDL_GLContext _ctx{nullptr};
+  bool _destroyed{false};
 };
 
 #endif // WINDOW_SDL_H
